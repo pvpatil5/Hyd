2a03979fb811ae7e46aca874f93df0072f33e9f1
Moulibasha <moulibasha4@gmail.com>
2021-11-12 17:42:55 +0530

    New commt

---
+++ src/test/java/Mouli/Mouli.java
@@ -0,0 +1,10 @@
+package Mouli;
+
+public class Mouli {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
+	
---
+++ src/test/java/Mouli/Sma.java
@@ -0,0 +1,7 @@
+package Mouli;
+
+public class Sma {
+public static void main(String[] args) {
+	System.out.println("hu");
+}
+}
