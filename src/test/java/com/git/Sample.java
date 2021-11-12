bd604b44b60d81d5e5d416c3db8d7ab5979763d8
pvpatil5 <pvpatil123@yahoo.com>
2021-11-12 16:46:08 +0530

    msg

---
+++ src/test/java/com/git/Sample.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("this is class 1");
+
+	}
+
+}
---
+++ src/test/java/com/git/Sample2.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		System.out.println("this is class 2");
+
+	}
+
+}
